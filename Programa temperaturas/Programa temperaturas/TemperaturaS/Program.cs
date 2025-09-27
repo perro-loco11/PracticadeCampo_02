8e67c77909267e53035a6f4a12ab2de81c08e894
Diego <N00481603@upn.pe>
2025-09-27 11:33:34 -0500

    Se ingresa programa de temperaturas

---
+++ Programa temperaturas/Programa temperaturas/TemperaturaS/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperaturaS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double celcius, fahrenheit, kelvin;
+
+            Console.Write("Ingrese la temperatura en grados celcius: ");
+            celcius = double.Parse(Console.ReadLine());
+
+            fahrenheit = (celcius * 9 / 5) + 32;
+            kelvin = (celcius + 273.15);
+
+            Console.WriteLine("La temperaura en fahrenheit es: {0}", fahrenheit);
+            Console.WriteLine("La temperatura en kelvin es: {0}", kelvin);
+
+            Console.ReadKey();
+        }
+    }
+}
